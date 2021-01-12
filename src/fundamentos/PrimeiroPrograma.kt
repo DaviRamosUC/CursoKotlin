891160cc1cbd414ce841f4079a71f07b0099f800
Davi <davi.lima@ucsal.edu.br>
2021-01-12 12:18:12 -0300

    Aprendendo Comentários

--- src/fundamentos/PrimeiroPrograma.kt
+++ src/fundamentos/PrimeiroPrograma.kt
@@ -1,6 +1,9 @@
 package fundamentos
 
 fun main(args: Array<String>) {
+    {
+        print("Primeiro") // não executa o código dentro de outro bloco
+    }
     print("Primeiro")
     println("Programa")
 }
